501846b31e17a419d966cfd6f36a9e06fd92194f
hopeless-zhao <wuzuishi@163.com>
2022-07-24 20:43:28 +0800

    add web container

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName:TestController
+ * @Description:TODO
+ * @author:zgw
+ * @date:2022/7/24 20:42
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping("/info")
+    public String hello(){
+        return "hello,world";
+    }
+}
